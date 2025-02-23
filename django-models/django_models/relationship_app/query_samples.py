2baba5ce0b2454cb5fc0f4f7fd25907f2088d3d2
Liliose-Gashugi <gashugililiose@gmail.com>
2025-02-23 23:30:30 +0200

    Added models

---
+++ django-models/django_models/relationship_app/query_samples.py
@@ -0,0 +1,22 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+def books_by_author(author_name):
+    author = Author.objects.filter(name=author_name).first()
+    if author:
+        return author.books.all()
+    return []
+
+# List all books in a library
+def books_in_library(library_name):
+    library = Library.objects.filter(name=library_name).first()
+    if library:
+        return library.books.all()
+    return []
+
+# Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    library = Library.objects.filter(name=library_name).first()
+    if library:
+        return library.librarian
+    return None
