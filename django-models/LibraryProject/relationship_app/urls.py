fc29e8b424647d26137b2e8e0aa0389c2db3f046
Liliose-Gashugi <gashugililiose@gmail.com>
2025-02-26 17:55:35 +0200

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import list_books
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
